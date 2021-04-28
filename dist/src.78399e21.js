786f7109f5f87c94809a55fb31953ea03cc09f82
daniswhoiam <daniil.belazovschi@gmail.com>
2021-04-28 21:15:13 +0200

    Better implemented React Bootstrap

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -34983,7 +34983,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50936" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62165" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
